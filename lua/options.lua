da11364076240dc67d79c33d9c2255dd638aab13
Nga Nguyen <nga.nguyen@silabs.com>
2025-02-16 22:20:40 +0700

    update doc for configuration

--- lua/options.lua
+++ lua/options.lua
@@ -4,11 +4,11 @@ vim.g.maplocalleader = " "
 vim.opt.cursorline = true
 
 -- use spaces for tabs and whatnot
-vim.opt.tabstop = 2
+vim.opt.tabstop = 2 -- Defines how many spaces a tab character represents.
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 2 -- Determines the number of spaces for indentation
 vim.opt.shiftround = true
-vim.opt.expandtab = true
+vim.opt.expandtab = true -- Converts tabs to spaces
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
@@ -24,3 +24,7 @@ vim.opt.splitright = true
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- scroll
+vim.opt.scrolloff = 10
+vim.opt.scroll = 50
