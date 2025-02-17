8075be8112aa5cda95a9035b2c062d81c3772f59
Nga Nguyen <nga.nguyen@silabs.com>
2025-02-17 14:18:48 +0700

    support terraform syntax and formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,31 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "terraformls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.terraformls.setup({
+				cmd = { "terraform-ls", "serve" },
+				filetypes = { "hcl", "tf", "tfvars" },
+				settings = {},
+				root_dir = lspconfig.util.root_pattern(".git", ".terraform", ".gitignore"),
+			})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,18 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "typescript", "vue", "dockerfile", "yaml", "json", "xml" },
+			ensure_installed = {
+				"lua",
+				"javascript",
+				"typescript",
+				"vue",
+				"dockerfile",
+				"yaml",
+				"json",
+				"xml",
+				"terraform",
+				"hcl",
+			},
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
